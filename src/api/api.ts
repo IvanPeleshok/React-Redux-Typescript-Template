9f21d69ea6e784cc556d6a9bd766364b5d5ee0dd
Peleshok Ivan <peleshok.ivan@gmail.com>
2020-12-09 15:52:10 +0300

    added inteceptor

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,17 +1,53 @@
-import Axios, { CancelToken, AxiosError } from "axios"
+import axios, { CancelToken, AxiosError } from "axios"
+import Cookies from "js-cookie";
 import { AlertifyStatusEnum } from "../types/types"
 import { converToFormData } from "../utils/apiFunctions"
 import { showAlert } from "../utils/showAlert"
+import { userAPI } from "./auth-api"
 
 export let apiURL = ""
 
-export const instance = Axios.create({
+export const instance = axios.create({
   baseURL: `${apiURL}api/`,
   headers: {
     "Content-Type": "application/json",
     "API-KEY": "",
   },
 })
+
+instance.interceptors.response.use(
+  response => response,
+  error => {
+    if (
+      error.response.status !== 401 ||
+      !error.response.headers["token-expired"]
+    ) {
+      return Promise.reject(error.response.status);
+    }
+    const tokens = {
+      accessToken: Cookies.get("accessToken"), 
+      refreshToken: Cookies.get("refreshToken")
+    };
+    return axios
+      .post(`${apiURL}/auth/refresh`, tokens)
+      .then(response => {
+        Cookies.set("accessToken", response.data.accessToken);
+        Cookies.set("refreshToken", response.data.refreshToken);
+        instance.defaults.headers["Authorization"] = `Bearer ${response.data.accessToken}`;
+        error.response.config.headers[
+          "Authorization"
+        ] = `Bearer ${response.data.accessToken}`;
+        return axios(error.response.config);
+      })
+      .catch(error => {
+        console.log(error);
+        Cookies.remove("accessToken");
+        Cookies.remove("refreshToken");
+        delete instance.defaults.headers["Authorization"];
+        return Promise.reject(error.response.status);
+      });
+  }
+);
 
 export const setTokenForAPI = (token: string) => {
   instance.defaults.headers.Authorization = "Bearer " + token
